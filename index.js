84babcebf0f79319e6e74dc7f7cad909af0c8bf7
Mitchell Schneider <mitchschneider88@gmail.com>
2023-02-09 16:45:26 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+let showPerformances = () => {
+    const performances = document.getElementsByClassName('performances_group');  // get the class name.
+    const hideProjects = document.getElementsByClassName('projects_group');
+    const hideClients = document.getElementsByClassName('clients_group');
+    // toggle or show/hide element.
+    performances[0].style.display = performances[0].style.display == 'none' ? 'grid' : 'none';
+    hideProjects[0].style.display = 'none';
+    hideClients[0].style.display = 'none';
+}
+
+let showProjects = () => {
+    const projects = document.getElementsByClassName('projects_group');  // get the class name.
+    const hidePerformances = document.getElementsByClassName('performances_group');
+    const hideClients = document.getElementsByClassName('clients_group')
+    // toggle or show/hide element.
+    projects[0].style.display = projects[0].style.display == 'none' ? 'grid' : 'none';
+    hidePerformances[0].style.display = 'none';
+    hideClients[0].style.display = 'none';
+}
+
+let showClients = () => {
+    const clients = document.getElementsByClassName('clients_group');  // get the class name.
+    const hidePerformances = document.getElementsByClassName('performances_group');
+    const hideProjects = document.getElementsByClassName('projects_group')
+    // toggle or show/hide element.
+    clients[0].style.display = clients[0].style.display == 'none' ? 'block' : 'none';
+    hidePerformances[0].style.display = 'none';
+    hideProjects[0].style.display = 'none';
+}
+
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 10; /* The speed/duration of the effect in milliseconds */
+    
+function typeWriter() {
+    if (i < txt.length) {
+    document.getElementById("clients_list").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+    }
+}
+
+/*
+
+var button = document.getElementById('hover');
+var body = document.body;
+
+button.onmouseover = function() {
+	body.className = 'hovered';
+}
+
+button.onmouseout = function() {
+	body.className = '';
+}
+
+*/
